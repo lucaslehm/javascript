5575069429f0e215332173e48b6d088f61ce4c6a
Lucas Marques Lehm <111029219+lucaslehm@users.noreply.github.com>
2023-03-30 14:06:13 -0300

    exaula10

---
+++ aula010/index.js
@@ -0,0 +1,7 @@
+let s1 = "A filha da xuxa se chama sasha";
+console.log(s1[3]);
+console.log(s1.length); // tamanho da string
+console.log(s1.indexOf('xuxa')) // 12
+console.log(s1.replace('sasha', 'jefferson'))
+console.log(s1.slice(11, 16))//xuxa
+console.log(s1.toUpperCase())
