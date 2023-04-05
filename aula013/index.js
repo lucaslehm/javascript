badc4e3f896060e584bd2e015fd1bac4f6ffcd11
Lucas Marques Lehm <111029219+lucaslehm@users.noreply.github.com>
2023-04-05 13:20:02 -0300

    js

---
+++ aula013/index.js
@@ -0,0 +1,10 @@
+let a1 = ['João', 'Maria', 'José']
+
+a1[0] = 'Lucas'
+a1.push('Carlos')
+a1.unshift('Gabriel')
+a1[0] = 'Geovanna'
+console.log(a1)
+
+console.log(a1.slice(0, 3))
+
---
+++ aula014/index.js
@@ -0,0 +1,6 @@
+function soma(x, y) {
+    let result = x + y
+    return result
+}
+
+console.log(soma(1, 4))
