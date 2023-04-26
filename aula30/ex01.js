ffb13e5b916f43bf4da308db631fa2d6c0aa2c37
Lucas Marques Lehm <111029219+lucaslehm@users.noreply.github.com>
2023-04-26 14:47:43 -0300

    exercicios logica

---
+++ aula30/ex01.js
@@ -0,0 +1,11 @@
+// aula 41 - Exercicios
+
+// escreva uma função que recebe 2 numeros e retorne o maior deles
+
+function numeroMaior(num1, num2) {
+return num1 > num2 ? num1 : num2
+}
+
+const numMaior = (num1, num2) => num1 > num2 ? num1 : num2;
+
+console.log(numMaior(4, 7))
