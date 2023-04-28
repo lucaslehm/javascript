e3d8cbe6df8b369283c848f8a1352773107dca47
Lucas Marques Lehm <111029219+lucaslehm@users.noreply.github.com>
2023-04-27 23:20:41 -0300

    js

---
+++ aula032/assets/js/main.js
@@ -0,0 +1,19 @@
+function global() {
+    const timer = document.querySelector('.timer')
+    const iniciar = document.querySelector('.iniciar')
+    const pausar = document.querySelector('.pausar')
+    const zerar = document.querySelector('.zerar')
+
+    iniciar.addEventListener('click', function (e) {
+        alert('clicou')
+    })
+
+    pausar.addEventListener('click', function (e) {
+        alert('clicou')
+    })
+
+    zerar.addEventListener('click', function (e) {
+        alert('clicou')
+    })
+}
+global()
--- aula31/index.js
+++ aula31/index.js
@@ -2,7 +2,7 @@
 
 function retornaHora(data) {
     if (data && !(data instanceof Date)) { // se (data == true e não(é uma instancia de Date) {faça isso...}
-        throw new TypeError('Formato Invalido, esperando uma instancia de Date')
+        throw new TypeError('Formato Invalido, esperando uma instancia de Date') // indentifica/lansa o erro
     }
 
     if (!data) { // se data !== true, ou seja, não recebeu um argumento para a função.
@@ -18,11 +18,11 @@ function retornaHora(data) {
 }
 
 try { // tentativa de execução "perigosa", que pode dar erro.
-    console.log(retornaHora('a'))
-} catch(e) {
+    console.log(retornaHora('a')) // tenta executar a função
+} catch(e) { // captura o erro e lida com ele (o que fazer caso de erro) - erro na "variavel" 'e' que esta dentro dos parenteses
     console.log(e)
     console.log('resolvendo erro... a hora exata é:')
     console.log(retornaHora(new Date()))
-} finally {
+} finally { // sempre é executado.
     console.log('tenha um bom dia')
 }
